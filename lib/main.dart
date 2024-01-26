68f60b1ab40327cc89775ce46e20c3af7067f845
realtnt <theo@thetaphoto.co.uk>
2024-01-26 19:16:05 +0000

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flame/game.dart';
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flappy_bird/screens/game_over_screen.dart';
+import 'package:flappy_bird/screens/main_menu_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  final game = FlappyBirdGame();
+  runApp(
+    GameWidget(
+      game: game,
+      initialActiveOverlays: const [MainMenuScreen.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenuScreen(game: game),
+        'gameOver': (context, _) => GameOverScreen(game: game),
+      },
+    ),
+  );
+}
